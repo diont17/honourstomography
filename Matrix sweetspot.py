0c341938079966984bbb1b100de922a0603489d7
Dion Thomas <dion@gemini.staff.vuw.ac.nz>
2016-04-01 17:15:16 +1300

    Edgier samples

--- Matrix sweetspot.py
+++ Matrix sweetspot.py
@@ -18,6 +18,7 @@ import scipy as sci
 import matplotlib.pylab as plt
 import numpy.linalg as linalg
 from scipy.ndimage.filters import gaussian_filter
+from scipy.signal.windows import gaussian as gaussian_window
 
 import slideTomography_1d as tm
 #reload(tm)
@@ -27,23 +28,26 @@ swSptPadding=10
 
 sweetSpot= np.zeros(swSptSize + 2*swSptPadding)
 sweetSpot[swSptPadding:-swSptPadding]=1
-#sweetSpot=gaussian_filter(sweetSpot,5)
+sweetSpot=gaussian_filter(sweetSpot,5)
 
 t1d= tm.Tomography_1D(sweetSpot)
 
-calSize=300
-calSmp=np.zeros(calSize)
-calSmp[40:260]=2
-calSmp[150:200]=0
-calSmp[70:120]=0
-#
-#calSmp[100:130]=1
+calSize=400
+calSmp=2*np.ones(calSize)
+calSmp[40:50]=0
+calSmp[60:70]=0
+calSmp[80:100]=0
+calSmp[120:150]=0
+calSmp[180:220]=0
+calSmp[260:300]=0
 
-sg=10
+
+sg=3
 calSmp=gaussian_filter(calSmp,sg)
 
 np.random.seed(2)
-storednoise=40-80.0*np.random.rand(600)
+storednoise=5-10*np.random.rand(600)
+#storednoise=np.zeros(600)
 
 calSig=t1d.calcSignal(calSmp)
 calSig+=storednoise[:len(calSig)]
@@ -51,16 +55,17 @@ t1d.calibrate(calSmp,calSig)
 
 
 smpSize=500
-smpPadding=60
+smpPadding=2
 
 # Sine function
 storednoise=1-2*np.random.rand(smpSize)
 smp  = np.zeros([smpSize])
-smp[smpPadding:-smpPadding] =  np.sin(np.linspace(0,20*np.pi,smpSize-2*smpPadding))
+smp[smpPadding:-smpPadding] =  np.sin(np.linspace(0,5*np.pi,smpSize-2*smpPadding))
 
 sinesig = t1d.calcSignal(smp)
 #signal+=storednoise
 rec = t1d.reconstruct(sinesig)
 plt.plot(smp,label='o')
+plt.plot(sinesig*0.01,label='s/100')
 plt.plot(rec,label='rec')
 plt.legend()
