536ae8c06e658703169fbfa83d66756fb8c292f7
Dion Thomas <dion@gemini.staff.vuw.ac.nz>
2016-04-08 12:49:38 +1200

    First attempt at offset reconstruction

--- Matrix sweetspot.py
+++ Matrix sweetspot.py
@@ -5,14 +5,6 @@ Created on Wed Mar 23 20:58:05 2016
 @author: Dion
 """
 
-
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 23 09:42:33 2016
-
-@author: dion
-"""
-
 import numpy as np
 import scipy as sci
 import matplotlib.pylab as plt
@@ -22,6 +14,7 @@ from scipy.signal.windows import gaussian as gaussian_window
 
 import slideTomography_1d as tm
 #reload(tm)
+#%%
 
 swSptSize=70
 swSptPadding=15
@@ -31,10 +24,11 @@ sweetSpot[swSptPadding:-swSptPadding]=1
 sweetSpot=gaussian_filter(sweetSpot,5)
 #sweetSpot=gaussian_window(100,40)
 
-
 t1d1= tm.Tomography_1D(sweetSpot)
 t1d2=tm.Tomography_1D(sweetSpot)
 t1d3=tm.Tomography_1D(sweetSpot)
+
+#%%Training data 1: Block with lots of holes, different sizes
 
 calSize=500
 calSmp=2*np.ones(calSize)
@@ -55,8 +49,8 @@ storednoise=noisesize-noisesize*2*np.random.rand(600)
 
 calSig=t1d1.calcSignal(calSmp)
 calSig+=storednoise[:len(calSig)]
-t1d1.addTrainingData(np.append(np.zeros(50),calSmp),calSig)
 
+#Training data 2: Solid block
 calSmp2=np.zeros(calSize)
 calSmp2[50:450]=2
 calSmp2=gaussian_filter(calSmp2,sg)
@@ -65,9 +59,8 @@ storednoise=noisesize-noisesize*2*np.random.rand(600)
 calSig2=t1d2.calcSignal(calSmp2)
 calSig2+=storednoise[:len(calSig2)]
 
-t1d1.addTrainingData(np.append(np.zeros(50),calSmp),calSig)
-t1d2.addTrainingData(np.append(np.zeros(50),calSmp2),calSig2)
-
+#Training data 3: Block with one hole
+#
 calSmp3=np.zeros(calSize)
 calSmp3[100:200]=2
 calSmp3[300:400]=2
@@ -75,50 +68,76 @@ calSmp3[300:400]=2
 storednoise=noisesize-noisesize*2*np.random.rand(600)
 calSig3=t1d2.calcSignal(calSmp3)
 calSig3+=storednoise[:len(calSig3)]
+
+#%%Train t1d
+
 t1d1.addTrainingData(np.append(np.zeros(50),calSmp),calSig)
 
+t1d1.addTrainingData(np.append(np.zeros(50),calSmp),calSig)
+t1d2.addTrainingData(np.append(np.zeros(50),calSmp2),calSig2)
+
+t1d1.addTrainingData(np.append(np.zeros(50),calSmp3),calSig3)
+
+#Setup an array of 50 t1ds, which will be trained with data:signal offsets
+shiftedt1ds=list()
+for i in xrange(100):
+    shiftedt1ds.append(tm.Tomography_1D(sweetSpot))
+    shiftcalSmp2=np.append(np.zeros(100),calSmp2)[i:100+len(calSmp2)]
+    shiftedt1ds[i].addTrainingData(shiftcalSmp2,calSig2)
 
 
+#%%
+
+#Reconstruction Tests
 smpSize=500
 smpPadding=2
 
 # Sine function
-smp  = np.zeros([smpSize])
-smp[smpPadding:-smpPadding] =  np.sin(np.linspace(0,10*np.pi,smpSize-2*smpPadding))
+sinesmp  = np.zeros([smpSize])
+sinesmp[smpPadding:-smpPadding] =  2*np.sin(np.linspace(0,10*np.pi,smpSize-2*smpPadding))
 
-rounsample=np.array([0,0,0,0,0.5,0.6,0.8,1,1.5,2,3,4,5,7,9,12,15,18,19,20,21,21.5,22,22.5,23,23.2,23.1,23.1,23.2,23.3,23.2,23.1,28,23.2,23.2,22.5,22,21.5,
-                     21,20.5,20,19,17,15,12,9,7,5,4,3,2,1.5,1,1,0.8,0.4,0,0,0,0])
+rounsample=np.array([0,0,0,0,0.5,0.6,0.8,1,1.5,2,3,4,5,7,9,12,15,18,19,20,21,21.5,22,22.5,23,23.2,23.1,23.1,23.2,23.3,23.2,23.1,28,23.2,23.2,
+                     22.5,22,21.5,21,20.5,20,19,17,15,12,9,7,5,4,3,2,1.5,1,1,0.8,0.4,0,0,0,0])
 
-smp=np.zeros(60*3)
+rounsmp=np.zeros(60*3)
 fill=0
+rounsmp=np.zeros(smpSize)
 for i in range(60):
-    smp[fill:fill+2]=rounsample[i]
-    fill+=2
-    
-smp=calSmp
+    rounsmp[fill:fill+4]=rounsample[i]
+    fill+=4
+smp=rounsmp
 
 noisesize=0
 storednoise=noisesize-noisesize*2*np.random.rand(600)
+
 
 testsig = t1d1.calcSignal(smp)
 testsig+=storednoise[:len(testsig)]
 rec = t1d1.reconstruct(testsig)
 rec2= t1d2.reconstruct(testsig)
-#rec3=t1d3.reconstruct(testsig)
-avgrec=((rec+rec2)/2.0)
 
+avgrec= np.zeros_like(rec)
+shiftweight=np.exp(-0.5*((np.arange(0,100)-(50))/(20))**2)
 
+shiftrecs=np.zeros((100,len(rec)))
+rawshiftrecs=np.zeros((100,len(rec)))
+
+for i in range(len(shiftedt1ds)):
+    rawshiftrecs[i]=shiftedt1ds[i].reconstruct(testsig)
+    padrec=np.append(np.zeros(50),rawshiftrecs[i])
+#    shiftrecs[i]=padrec[i:i+len(rec)]
+    
+avgrec*=(1.0/len(shiftedt1ds))
+
+plt.figure()
 plt.plot(np.arange(50,50+len(smp)),smp,label='o')
-plt.plot(testsig*0.01,label='s/100')
-plt.plot(rec,label='rec')
-plt.plot(rec2,label='rec2')
-#plt.plot(rec3,label='rec3')
-plt.plot(avgrec,label='avg')
-
+#plt.plot(testsig*0.01,label='s/100')
+plt.plot(rec,label='rec (multiple trainings)')
+plt.plot(rec2,label='rec2 (1 training)')
+plt.plot(avgrec,label='rec (average of shifted trainings)')
 plt.legend()
 
-
-#plt.plot(calSig2/40,label='S/40')
-#plt.plot(np.append(np.zeros(50),calSmp),label='O')
-#plt.plot(t1d2.reconstruct(calSig),label='R')
-#plt.legend()
+plt.figure()
+for m in rawshiftrecs:
+    plt.plot(m)
+plt.plot(np.arange(50,50+len(smp)),smp)
