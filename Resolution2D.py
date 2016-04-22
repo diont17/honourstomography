a59895dbd8da4eabbd51c3c7de932e81ad95a590
Dion Thomas <dion@gemini.staff.vuw.ac.nz>
2016-04-22 14:06:13 +1200

    Resolution testing

---
+++ Resolution2D.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 22 10:02:34 2016
+
+@author: dion
+"""
+
+
+import numpy as np
+import scipy.ndimage as img
+from scipy.optimize import minimize
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+import TwoDTomography as tm
+
+
+def addNoise2D(arr, noisepercent):
+    arrx= arr.shape[1]
+    arry= arr.shape[0]
+    noise = noisepercent - 2*noisepercent*np.max(arr)*np.random.rand(arry,arrx)
+    return arr+noise
+
+def makeSweetSpot(std):
+    size=20
+    x, y = np.float64(np.mgrid[-size/2 + 1:size/2 + 1, -size/2 + 1:size/2 + 1])
+    x-=0.5
+    y-=0.5
+    #0.9 for narrow sweetspot
+    #0.4 for wide sweetspot
+    
+    sweetSpot=np.exp(- ((x**2 + y**2)/ 2*std**2))
+    sweetSpot*=(1.0/np.max(sweetSpot))
+    return sweetSpot
+    #sweetSpot=np.repeat(np.repeat(sweetSpot,2,axis=0),2,axis=1)
+    #sweetSpot=np.ones((6,6))
+
+t2ds=tm.Tomographer_2d(makeSweetSpot(0.9))
+t2dm=tm.Tomographer_2d(makeSweetSpot(0.4))
+t2dl=tm.Tomographer_2d(makeSweetSpot(0.2))
+
+
+np.random.seed(2)
+diagholes4= np.array([[0,0,0,0,1,1,1,1,1,1,1,1],[0,0,0,0,1,1,1,1,1,1,1,1],[0,0,0,0,1,1,1,1,1,1,1,1],[0,0,0,0,1,1,1,1,1,1,1,1],
+                [1,1,1,1,0,0,0,0,1,1,1,1],[1,1,1,1,0,0,0,0,1,1,1,1],[1,1,1,1,0,0,0,0,1,1,1,1],[1,1,1,1,0,0,0,0,1,1,1,1],
+                [1,1,1,1,1,1,1,1,0,0,0,0],[1,1,1,1,1,1,1,1,0,0,0,0],[1,1,1,1,1,1,1,1,0,0,0,0],[1,1,1,1,1,1,1,1,0,0,0,0]])
+diagholes12=np.repeat(np.repeat(diagholes4,3,axis=0),3,axis=1)
+
+square6=np.array([[1,1,1,1,1,1],[1,1,1,1,1,1],[1,1,1,1,1,1],[1,1,1,1,1,1],[1,1,1,1,1,1],[1,1,1,1,1,1]])
+square12=np.repeat(np.repeat(square6,2,axis=0),2,axis=1)
+
+ring12=img.imread('whitering.png', flatten=True)[::5,::5]
+phan=img.imread('sheplog.png', flatten=True) [::1,::1]
+ring12/=255.0
+square12=ring12
+
+#%%Training
+trainingnoise=0.01
+t2ds.addTrainingData(square12,addNoise2D(t2ds.calcSignal(square12),trainingnoise))
+t2dm.addTrainingData(square12,addNoise2D(t2dm.calcSignal(square12),trainingnoise))
+t2dl.addTrainingData(square12,addNoise2D(t2dl.calcSignal(square12),trainingnoise))
+
+t2ds.calibrate()
+t2dm.calibrate()
+t2dl.calibrate()
+
+#%%Reconstruct
+noise=0.01
+ssdh4 = addNoise2D(t2ds.calcSignal(diagholes4),noise)
+ssdh12= addNoise2D(t2ds.calcSignal(diagholes12),noise)
+ssrng12= addNoise2D(t2ds.calcSignal(ring12),noise)
+ssphan=addNoise2D(t2ds.calcSignal(phan),noise)
+
+srdh4 = t2ds.reconstruct(ssdh4)
+srdh12= t2ds.reconstruct(ssdh12)
+srrng12= t2ds.reconstruct(ssrng12)
+srphan=t2ds.reconstruct(ssphan)
+
+msdh4 = addNoise2D(t2dm.calcSignal(diagholes4),noise)
+msdh12= addNoise2D(t2dm.calcSignal(diagholes12),noise)
+msrng12= addNoise2D(t2dm.calcSignal(ring12),noise)
+msphan= addNoise2D(t2dm.calcSignal(phan),noise)
+
+mrdh4 = t2dm.reconstruct(msdh4)
+mrdh12= t2dm.reconstruct(msdh12)
+mrrng12= t2dm.reconstruct(msrng12)
+mrphan= t2dm.reconstruct(msphan)
+
+lsdh4 = addNoise2D(t2dl.calcSignal(diagholes4),noise)
+lsdh12= addNoise2D(t2dl.calcSignal(diagholes12),noise)
+lsrng12= addNoise2D(t2dl.calcSignal(ring12),noise)
+lsphan=addNoise2D(t2dl.calcSignal(phan),noise)
+
+lrdh4 = t2dl.reconstruct(lsdh4)
+lrdh12= t2dl.reconstruct(lsdh12)
+lrrng12= t2dl.reconstruct(lsrng12)
+lrphan= t2dl.reconstruct(lsphan)
+
+fig1,(ax11,ax12,ax13)=plt.subplots(nrows=1,ncols=3,figsize=(15,5))
+ms11   = ax11.matshow(t2ds.sweetSpot)
+ax11.set_title('Sweet Spot -small')
+ms12   = ax12.matshow(t2dm.sweetSpot)
+ax12.set_title('Sweet Spot -med')
+ms13=ax13.matshow(t2dl.sweetSpot)
+ax13.set_title('SweetSpot-large')
+div13  = make_axes_locatable(ax13)
+cax13  = div13.append_axes("right", size="20%", pad=0.05)
+cbar13 = plt.colorbar(ms11)
+cax13.set_visible(False)
+
+fig2,(ax21,ax22,ax23)=plt.subplots(nrows=1,ncols=3,figsize=(15,5))
+ms21=ax21.matshow(srdh4)
+ax21.set_title('R-diaghole4-small')
+ms22=ax22.matshow(mrdh4)
+ax22.set_title('R-Diaghole4-med')
+ms23=ax23.matshow(lrdh4)
+ax23.set_title('R-Diaghole4-large')
+plt.colorbar(ms21)
+
+fig3,((ax31,ax32,ax33),(ax31b,ax32b,ax33b))=plt.subplots(nrows=2,ncols=3,figsize=(15,10))
+ms31=ax31.matshow(srdh12)
+ax31.set_title('R-diaghole12-small')
+ms32=ax32.matshow(mrdh12)
+ax32.set_title('R-Diaghole12-med')
+ms33=ax33.matshow(lrdh12)
+ax33.set_title('R-Diaghole12-large')
+plt.colorbar(ms31)
+ax31b.matshow(ssdh12)
+ax31b.set_title('S-diaghole12-small')
+ax32b.matshow(msdh12)
+ax32b.set_title('S-diaghole12-med')
+ax33b.matshow(lsdh12)
+ax33b.set_title('S-diaghole12-large')
+
+fig4,((ax41,ax42,ax43),(ax41b,ax42b,ax43b))=plt.subplots(nrows=2,ncols=3,figsize=(15,5))
+ms41=ax41.matshow(srrng12)
+ax41.set_title('R-ring12-small')
+ms42=ax42.matshow(mrrng12)
+ax42.set_title('R-ring12-med')
+ms43=ax43.matshow(lrrng12)
+ax43.set_title('R-Ring12-large')
+plt.colorbar(ms41)
+ax41b.matshow(ssrng12)
+ax42b.matshow(msrng12)
+ax43b.matshow(lsrng12)
+
+fig5,ax5=plt.subplots(nrows=2,ncols=3,figsize=(15,5))
+ms51=ax5[0,0].matshow(srphan)
+ax5[0,0].set_title('R-phantom-small')
+ms52=ax5[0,1].matshow(mrphan)
+ax5[0,1].set_title('R-phantom-med')
+ms53=ax5[0,2].matshow(lrphan)
+ax5[0,2].set_title('R-phantom-large')
+plt.colorbar(ms51)
+ax5[1,0].matshow(ssphan)
+ax5[1,1].matshow(msphan)
+ax5[1,2].matshow(lsphan)
--- TwoDTomography.py
+++ TwoDTomography.py
@@ -9,9 +9,6 @@ import numpy as np
 import scipy.ndimage as img
 from scipy.optimize import minimize
 
-import matplotlib.pyplot as plt
-from matplotlib import cm
-
 class Tomographer_2d(object):
     
     def __init__(self,sweetSpot):
@@ -49,7 +46,7 @@ class Tomographer_2d(object):
                 segment=padsmp[padm-lby:padm+lby, padn-lbx:padn+lbx]
                 sig[m,n]=np.sum(segment*self.sweetSpot)
 
-        return sig,padsmp
+        return sig
     
     def addTrainingData(self, smp, sig):
         lbx=int(self.Nx/2)
@@ -70,7 +67,7 @@ class Tomographer_2d(object):
         #pad sample so looking back works                
         padsig=np.zeros([self.Ny + Sy, self.Nx+Sx])
 #        padsig[lby:lby+Sy, lbx:lbx+Sx]=sig
-        padsig[0:Sy, 0:Sx]=sig
+        padsig[lby:Sy+lby, lbx:Sx+lbx]=sig
         segment=np.zeros((self.Ny,self.Nx))
         
         for m in xrange(0,Sy):
@@ -83,18 +80,18 @@ class Tomographer_2d(object):
                 newb[(m*Sx) +n] = padSmp[m,n]
             
         
-        self.Amatrix=np.concatenate((self.Amatrix,newA),axis=0)
-        self.b=np.concatenate((self.b,newb),axis=0)
+        self.Amatrix=np.concatenate((self.Amatrix,newA[:]),axis=0)
+        self.b=np.concatenate((self.b,newb[:]),axis=0)
 
 
     def calibrate(self):
-#        self.CA,_,_,_= np.linalg.lstsq(self.Amatrix,self.b)
-#        self.CA=self.CA.reshape((self.Ny,self.Nx))
-        self.optres=minimize(self.lossfn,x0=np.ones(self.Nx*self.Ny))
-        self.CA=self.optres.x.reshape((self.Ny,self.Nx))
+        self.CA0,_,_,_= np.linalg.lstsq(self.Amatrix,self.b)
+#        self.optres=minimize(self.lossfn,x0=0.3*np.ones(self.Nx*self.Ny))
+#        self.CA=self.optres.x.reshape((self.Ny,self.Nx))
+        self.CA=self.CA0.reshape((self.Ny,self.Nx))
         
     def lossfn(self,CAx):
-        return np.sum((np.dot(self.Amatrix,CAx)-self.b)**2) + (self.lam) * np.sum((CAx)**2)
+        return 1e-3*np.sum((np.dot(self.Amatrix,CAx)-self.b)**2) + 1e-3*(self.lam) * np.sum((CAx)**2)
 #        return np.sum(np.abs(out-self.b)) + 80*np.sum(np.abs(np.diff(CAx)))
                 
         
@@ -103,92 +100,3 @@ class Tomographer_2d(object):
         rec=img.convolve(sig,weights=self.CA,mode='constant', cval=0)
         return rec
 
-def addNoise2D(arr, noisepercent):
-    arrx= arr.shape[1]
-    arry= arr.shape[0]
-    noise = noisepercent - 2*noisepercent*np.max(arr)*np.random.rand(arry,arrx)
-    return arr+noise
-
-np.random.seed(2)
-size=10
-x, y = np.float64(np.mgrid[-size/2 + 1:size/2 + 1, -size/2 + 1:size/2 + 1])
-x-=0.5
-y-=0.5
-#0.9 for narrow sweetspot
-#0.4 for wide sweetspot
-std=0.4
-
-sweetSpot=np.exp(- ((x**2 + y**2)/ 2*std**2))
-sweetSpot*=(1.0/np.max(sweetSpot))
-#sweetSpot=np.ones((6,6))
-t2d=Tomographer_2d(sweetSpot)
-
-#smp=img.imread('whitesquare.png',flatten=True)[::80,::80]
-smp1= np.array([[1,1,1,1,1,1],[1,1,1,1,1,1],[1,1,0,0,1,1],[1,1,0,0,1,1],[1,1,1,1,1,1],[1,1,1,1,1,1]])
-smp1= np.array([[0,0,0,0,1,1,1,1,1,1,1,1],[0,0,0,0,1,1,1,1,1,1,1,1],[0,0,0,0,1,1,1,1,1,1,1,1],[0,0,0,0,1,1,1,1,1,1,1,1],
-                [1,1,1,1,0,0,0,0,1,1,1,1],[1,1,1,1,0,0,0,0,1,1,1,1],[1,1,1,1,0,0,0,0,1,1,1,1],[1,1,1,1,0,0,0,0,1,1,1,1],
-                [1,1,1,1,1,1,1,1,0,0,0,0],[1,1,1,1,1,1,1,1,0,0,0,0],[1,1,1,1,1,1,1,1,0,0,0,0],[1,1,1,1,1,1,1,1,0,0,0,0]])
-
-smp2=np.array([[1,1,1,1,1,1],[1,1,1,1,1,1],[1,1,1,1,1,1],[1,1,1,1,1,1],[1,1,1,1,1,1],[1,1,1,1,1,1]])
-smp3=img.imread('whitering.png', flatten=True)[::10,::10]
-smp4=img.imread('stripes.png', flatten=True) [::10,::2]
-smp3/=255.0
-smp4/=255.0
-smp1=np.repeat(np.repeat(smp1,4,axis=0),4,axis=1)
-
-sig1,padsmp1=t2d.calcSignal(smp1)
-sig2,_=t2d.calcSignal(smp2)
-sig3,_=t2d.calcSignal(smp3)
-sig4,_=t2d.calcSignal(smp4)
-
-#plt.matshow(padsmp)
-#plt.matshow(smp,cmap=cm.gray)
-#plt.title('sample')
-#plt.figure()
-#plt.matshow(sig,cmap=cm.gray)
-#plt.title('signal')
-
-#t2d.addTrainingData(smp1,addNoise2D(sig1,0.05))
-#t2d.addTrainingData(smp1,addNoise2D(sig1,0.11))
-#t2d.addTrainingData(smp1,addNoise2D(sig1,0.21))
-
-t2d.addTrainingData(smp2,addNoise2D(sig2,0.01))
-t2d.addTrainingData(smp2,addNoise2D(sig2,0.01))
-t2d.addTrainingData(smp2,addNoise2D(sig2,0.01))
-t2d.lam=16000
-t2d.calibrate()
-
-plt.figure()
-plt.matshow(sweetSpot)
-plt.title('Sweet Spot')
-plt.colorbar()
-
-plt.figure()
-plt.matshow(t2d.CA,cmap=cm.gray)
-plt.title('Inverse Filter')
-plt.colorbar()
-testnoise=0.4
-rec2=t2d.reconstruct(addNoise2D(sig2,testnoise))
-#rec2+=(-1.0*rec2.min())
-plt.figure()
-plt.matshow(rec2,cmap=cm.gray)
-plt.title('Recovered Sample - 6x6 block')
-plt.colorbar()
-
-rec1=t2d.reconstruct(addNoise2D(sig1,testnoise))
-plt.figure()
-plt.matshow(rec1,cmap=cm.gray)
-plt.title('Recovered sample- hole=4')
-plt.colorbar()
-
-rec3=t2d.reconstruct(addNoise2D(sig3,testnoise))
-plt.figure()
-plt.matshow(rec3,cmap=cm.gray)
-plt.colorbar()
-plt.title('Recovered - ring, hole=10')
-
-rec4=t2d.reconstruct(addNoise2D(sig4,testnoise))
-plt.figure()
-plt.matshow(rec4)
-plt.colorbar()
-plt.title('Recovered - stripes 5px:11px')
